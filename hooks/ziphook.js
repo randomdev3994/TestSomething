bd8318b9c27a8acdd53895b087492372745bac9e
randomdev3994 <111250354+randomdev3994@users.noreply.github.com>
2025-02-01 22:26:12 +0000

    Update ziphook.js

--- hooks/ziphook.js
+++ hooks/ziphook.js
@@ -1,27 +1,52 @@
-const fs = require('fs');
-const path = require('path');
-const AdmZip = require('adm-zip');
+import fs from 'fs';
+import path from 'path';
+import AdmZip from 'adm-zip';
 
 const zipFile = () => {
-  // Source file path and destination paths
-  const sourceFilePath = path.join(__dirname, '..', 'src', ''); // Adjust path as needed
-  //const outputDir = path.join(__dirname, '..', 'dist', 'assets'); // Target directory inside the plugin
-  const outputDir = path.resolve(process.cwd()); // Main app directory
-  const zipFileName = 'myfile.zip'; // Desired zip file name
-  const zipFilePath = path.join(outputDir, zipFileName);
+  const platform = process.env.CAPACITOR_PLATFORM_NAME;
+  const __dirname = path.resolve(process.cwd());
+  console.log(platform);
+  const sourcePath = path.join(__dirname, '..', '..', (platform === 'ios' ? 'ios' : 'android'), '');
+  const outputDir = path.resolve(process.cwd(), '..', '..');
+  const zipFileName = platform === 'ios' ? 'src-ios.zip' : 'src-android.zip';
+
+  const zip = new AdmZip();
 
   console.log(outputDir);
-  // Ensure the output directory exists
-  if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir, { recursive: true });
+
+  const addFolderToZip = (folderPath, zip, zipFolderPath = '') => {
+
+    const items = fs.readdirSync(folderPath);
+  
+    items.forEach(item => {
+      const itemPath = path.join(folderPath, item);
+      if(fs.existsSync(itemPath)) {
+      const itemZipPath = path.join(zipFolderPath, item);
+  
+      // Check if the current item is a directory or a file
+      const stats = fs.statSync(itemPath);
+  
+      if (stats.isDirectory()) {
+        // If it's a directory, recurse
+        addFolderToZip(itemPath, zip, itemZipPath);
+      } else {
+        // If it's a file, add it to the zip
+        zip.addLocalFile(itemPath, zipFolderPath);
+      }
+      }
+    });
+  };
+
+  if (fs.existsSync(sourcePath) && (platform === 'ios' || platform === 'android')) {
+    addFolderToZip(sourcePath, zip);
+  } else {
+    return;
   }
 
-  // Create a zip archive
-  const zip = new AdmZip();
-  zip.addLocalFile(sourceFilePath);
+  const zipFilePath = path.join(outputDir, zipFileName);
   zip.writeZip(zipFilePath);
 
   console.log(`File zipped successfully at: ${zipFilePath}`);
 };
-console.log('START')
+console.log('START ZIP')
 zipFile(); // Execute the zipping logic
