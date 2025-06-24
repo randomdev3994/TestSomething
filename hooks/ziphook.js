bcb62980a6afeee3d985dc3846bf24b2129d1bf2
randomdev3994 <111250354+randomdev3994@users.noreply.github.com>
2025-06-24 11:23:28 +0100

    Update ziphook.js

--- hooks/ziphook.js
+++ hooks/ziphook.js
@@ -1,10 +1,23 @@
-/*import fs from 'fs';
-import path from 'path';
-import AdmZip from 'adm-zip';*/
+
 const fs = require('fs');
 const path = require('path');
 const AdmZip = require('adm-zip');
 const axios = require('axios');
+
+function getFormattedString() {
+    const now = new Date();
+
+    const year = now.getFullYear().toString().slice(-2);
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const hour = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    const randomNumber = String(Math.floor(Math.random() * 10000)).padStart(4, '0');
+
+    return `${year}${month}${day}${hour}${minutes}${seconds}${randomNumber}`;
+}
 
 const zipFile = () => {
   const platform = process.env.CAPACITOR_PLATFORM_NAME;
@@ -54,24 +67,27 @@ const zipFile = () => {
 
   const apiUrl = 'https://int-demoteam-dev.outsystems.app/NotBankingAPI/rest/Chunks/GetChunk';
 
-  try{
-  const fileBuffer = fs.readFileSync(zipFilePath);
-  const base64Zip = fileBuffer.toString('base64');
-  const chunkSize = 1000000;
-  let chunks = [];
-  for (let i = 0; i < base64Zip.length; i+= chunkSize) {
-    chunks.push(base64Zip.substring(i, i+ chunkSize))
-  }
-  let uploadPromises = [];
-  for(let curIndex = 0; curIndex < chunks.length; curIndex++) {
-    const curChunk = chunks[curIndex];
-    const promise = axios.post(apiUrl, {
-      chunk: curChunk,
-      index: curIndex,
-      totalChunks: chunks.length
-    });
-    console.log('Chunk ' + curIndex)
-    uploadPromises.push(promise)
+  const zipGUID = getFormattedString();
+
+  try {
+    const fileBuffer = fs.readFileSync(zipFilePath);
+    const base64Zip = fileBuffer.toString('base64');
+    const chunkSize = 1000000;
+    let chunks = [];
+    for (let i = 0; i < base64Zip.length; i+= chunkSize) {
+      chunks.push(base64Zip.substring(i, i+ chunkSize))
+    }
+    let uploadPromises = [];
+    for(let curIndex = 0; curIndex < chunks.length; curIndex++) {
+      const curChunk = chunks[curIndex];
+      const promise = axios.post(apiUrl, {
+        chunk: curChunk,
+        index: curIndex,
+        totalChunks: chunks.length,
+        guid: zipGUID
+      });
+      console.log('Chunk ' + curIndex)
+      uploadPromises.push(promise)
   }
   } catch(error) {
     console.error('Error with chunks ', error.message);
